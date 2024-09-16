543007402e6626891ba73099633dd48ddd338959
Nazmul Hoque <sujon686500@gmail.com>
2024-09-16 22:00:08 +0600

    banners left side complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Check } from "lucide-react";
 
 export default function Home() {
   return (
@@ -8,8 +9,36 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <img src="/snake-1.png" alt="" />
+                <img className="w-full" src="/snake-1.png" alt="snake image" />
               </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your Image on a{" "}
+                <span className="bg-green-600 px2 text-white">Custom</span>{" "}
+                Phone Case
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Capture your favourite memories with your own,{" "}
+                <span className="font-semibold">one-of-one</span> phone case.
+                CaseViper allows to protect your memories, not just your phone
+                case.{" "}
+              </p>
+
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="size-5 shrink-0 text-green-600" />
+                    High-quality durable material
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="size-5 shrink-0 text-green-600" />5 years
+                    print gurantee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="size-5 shrink-0 text-green-600" />
+                    Modern iPhone models supported
+                  </li>
+                </div>
+              </ul>
             </div>
           </div>
         </MaxWidthWrapper>
