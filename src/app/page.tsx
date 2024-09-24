bb23ab4b548e6dc2d1f9d11246e0198cd04224fe
Nazmul Hoque <sujon686500@gmail.com>
2024-09-24 10:54:17 +0600

    auth with kinde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
             <div className="relative md:max-w-xl">
               <img
                 src="/your-image.png"
-                className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
+                className="absolute w-40 lg:w-52 left-48 -top-20 select-none hidden sm:block md:hidden xl:block"
               />
               <img
                 src="/line.png"
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,17 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import React from "react";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 interface NavbarProps {
   // Define props here
 }
 
-const Navbar: React.FC<NavbarProps> = ({}) => {
-  const user = undefined;
-  const isAdmin = undefined;
+const Navbar: React.FC<NavbarProps> = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
 
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-b-gray-200 bg-white/70 backdrop-blur-lg transition-all">
